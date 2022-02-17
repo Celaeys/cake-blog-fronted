69e5bdbb9dd80135acbcb87010b3705a8e934b71
Raphaela Seibert <raphaelaseibert@gmail.com>
2022-02-17 20:31:25 +0100

    got blog data need to figure out the image thing

--- src/blogData.js
+++ src/blogData.js
@@ -3,20 +3,37 @@ const blogData = getBlogData();
 // api call TODOOO STUFF
 
 async function getBlogData() {
-    fetch('http://h2952311.stratoserver.net/wp-json/wp/v2/')
-    .then(response => {
+    fetch('http://h2952311.stratoserver.net/wp-json/wp/v2/posts')
+    .then(response => response.json())
+    .then(data => {
         // taxonomy category name render for id in posts and check for name
-        let posts = response.data;
+        let posts = data;
         for (let i = 0; i < posts.length; i++) {
           posts[i].headline = posts[i].title.rendered;
           posts[i].description = posts[i].excerpt.rendered;
-          console.log(posts)
-          // getMediaUrl(posts[i].featured_media, i);
-          // getDate(posts[i].date, i);
+          getMediaUrl(posts[i].featured_media, i);
+          // .then((imageURL) => { posts[i].image = imageURL});
+          posts[i].dateBeautified = getDate(posts[i].date, i);
         }
+        console.log('POSTS: ', data)
         return posts;
     })
-    // const myJson = await response.json();
+}
+
+function getMediaUrl(mediaId) {
+  fetch('http://h2952311.stratoserver.net/wp-json/wp/v2/' + 'media/' + mediaId)
+    .then(response => response.json())
+    .then(data => {
+      console.log('IMAGE: ', data.source_url)
+       return data.source_url;
+    })
+}
+
+function getDate(date) {
+  const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+  let dateObject = new Date(date)
+  console.log(dateObject.getDay())
+  return dateObject.getDay() + ' ' + monthNames[dateObject.getMonth()];
 }
 
 export default blogData;
